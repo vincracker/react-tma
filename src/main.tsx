9fa487ee3244905537fb8222bb05a08bdef58134
Vincentchen <vincentchen0930@gmail.com>
2025-01-08 12:07:34 +0800

    add telegram

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { AppRoot } from "@telegram-apps/telegram-ui";
+import App from "./App.tsx";
+import { init } from "./init.tsx";
+import { retrieveLaunchParams } from "@telegram-apps/sdk-react";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = createRoot(document.getElementById("root")!);
+
+try {
+  const launchParams = retrieveLaunchParams();
+  init(launchParams.startParam === "debug" || import.meta.env.DEV);
+
+  root.render(
+    <StrictMode>
+      <AppRoot>
+        <App />
+      </AppRoot>
+    </StrictMode>
+  );
+} catch {
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}
